7f9036ec1e85eff01c91c3aee0cd20106655d07e
Daniel C <brikbusters@gmail.com>
2025-01-20 16:31:28 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # webcamit
-Replacement for EOS Webcam Utility
+A linux daemon that automatically sets up and handles PTP cameras as video devices through v4l2.
+
+- [x] Working basic webcam with ffmpeg
+- [x] Listen to USB events and connect to a camera if found
+- [ ] Monitor processes that use the v4l2 device and pause/end PTP thread if there are none
+- [ ] Support more than one camera
+- [ ] Automatically handle v4l2 options if needed
+- [ ] UI with options to change camera settings while webcam is active
--- scripts.sh
+++
@@ -1,8 +0,0 @@
-sudo modprobe v4l2loopback devices=1 video_nr=7 exclusive_caps=1 card_label="webcamit"
-
-rmmod v4l2loopback 2> /dev/null
-gphoto2 --stdout --capture-movie | gst-launch-1.0 fdsrc ! image/jpeg,framerate=25/1,format=YUY2,width=1024,height=680 ! decodebin3 name=dec ! queue ! videoconvert ! tee ! v4l2sink device=/dev/video5 sync=false
-ffmpeg -f x11grab -r 30 -s 1366x768 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video6
-
-ffmpeg -f mjpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f sdl2 "Output Window"
-ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video6
